2046b0f15e134aa0b25ca525c8511b48788dc9ed
Smishra2711 <mishrashubham133@gmail.com>
2021-09-13 17:49:44 +0530

    adding master service

---
+++ src/app/service/master-service.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class MasterServiceService {
+  constructor(private http: HttpClient) {}
+
+  apiServer = "https://api.vettyonline.com/";
+
+  post(apiPath: string, body: any) {
+    const headers = { "content-type": "application/json" };
+    return this.http.post(this.apiServer + apiPath, body, { headers: headers });
+  }
+
+  get(apiPath: string) {
+    const headers = {
+      "content-type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token")}`
+    };
+    return this.http.get(this.apiServer + apiPath, { headers: headers });
+  }
+
+  authPost(apiPath: string, body: any) {
+    const headers = { "content-type": "application/json" };
+    return this.http.post(this.apiServer + apiPath, body, { headers: headers });
+  }
+
+  authGet(apiPath: string) {
+    const headers = {
+      "content-type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token")}`
+    };
+    return this.http.get(this.apiServer + apiPath, { headers: headers });
+  }
+}
